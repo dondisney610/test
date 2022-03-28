92e6ec4353b6bff10ac8548bde2afa89ea9f9483
rikudon <84581072+dondisney610@users.noreply.github.com>
2022-03-28 20:05:19 +0900

    Delete assets directory

--- assets/js/item.js
+++
@@ -1,5 +0,0 @@
-$(".category_img").click(function () {
-    const mainImg = $("#main_img").children("img");
-    const imgSrc = $(this).children("img").attr("src");
-    mainImg.attr("src", imgSrc);
-});
